37bae4619f3e123058498e706126d72207f79da3
Jiancao164 <60164697+Jiancao164@users.noreply.github.com>
2021-01-12 15:49:53 -0800

    update the solution

--- CommandTest.py
+++ CommandTest.py
@@ -5,17 +5,23 @@ from main import solution
 
 class TestCommand(unittest.TestCase):
     def test_solution(self):
-        self.assertEqual(solution("/", "abc"), "/abc")
-        self.assertEqual(solution("/abc/def", "ghi"), "/abc/def/ghi")
-        self.assertEqual(solution("/abc/def", ".."), "/abc")
-        self.assertEqual(solution("/abc/def", "/abc"), "/abc")
-        self.assertEqual(solution("/abc/def", "../.."), "/")
-        self.assertEqual(solution("/abc/def", "../../.."), "/")
-        self.assertEqual(solution("/abc/def", "."), "/abc/def")
-        self.assertEqual(solution("/abc/def", "..klm"), "No such file or directory")
-        self.assertEqual(solution("/abc/def", "//////"), "/")
-        self.assertEqual(solution("/abc/def", "......"), "No such file or directory")
-        self.assertEqual(solution("/abc/def", "../gh///../klm/."), "/abc/klm")
-        self.assertEqual(solution("abc/def", "../gh///../klm/."), "/abc/klm")
+        self.assertEqual("/abc", solution("/", "abc"))
+        self.assertEqual("/abc/def/ghi", solution("/abc/def", "ghi"))
+        self.assertEqual("/abc", solution("/abc/def", ".."))
+        self.assertEqual("/abc", solution("/abc/def", "/abc"))
+        self.assertEqual("/abc/klm", solution("/abc/def", "/abc/klm"))
+        self.assertEqual("/", solution("/abc/def", "../.."))
+        self.assertEqual("/", solution("/abc/def", "../../.."))
+        self.assertEqual("/abc/def", solution("/abc/def", "."))
+        self.assertEqual("No such file or directory", solution("/abc/def", "..klm"))
+        self.assertEqual("/", solution("/abc/def", "//////"))
+        self.assertEqual("No such file or directory", solution("/abc/def", "......"))
+        self.assertEqual("/abc/klm", solution("/abc/def", "../gh///../klm/."))
+
+        self.assertEquals("/a/b/c/d", solution("/a/b/c", "d/"))
+        self.assertEquals("/l/n/m/o", solution("/l/n/m", "o"))
+        self.assertEquals("/", solution("/a/b/c", "/./../a/../b/../"))
+        self.assertEquals("/a/c", solution("/a/b/c", "../../c/"))
+        self.assertEquals("/a/b/b/c", solution("/a/b/c", "../b/c"))
 
 
--- main.py
+++ main.py
@@ -1,98 +1,63 @@
-
-# A class to simulate a directory in the system
-class Folder:
-    def __init__(self, name):
-        self.name = name
-    subFolders = []
-    parent = ""
-    name = ""
-
- 
-"""
-simulate the following directory in system
-         |--def -- ghi
-    abc--|--klm
-         |--gh
-    
-    tmt--|
-"""
-root = Folder("mycd")
-p1 = Folder("abc")
-p1.parent = root
-root.subFolders.append(p1)
-p2 = Folder("def")
-p2.parent = p1
-p1.subFolders.append(p2)
-p3 = Folder("klm")
-p3.parent = p1
-p1.subFolders.append(p3)
-p4 = Folder("gh")
-p1.subFolders.append(p4)
-p4.parent = p1
-p5 = Folder("ghi")
-p5.parent = p2
-p2.subFolders.append(p5)
-
-
 def solution(current_directory, new_directory):
     # check if the input is valid
     if new_directory is None or new_directory == "":
         return ""
-    folders = current_directory.split("/")
-    directories = []
-    path = root
-    # move the path's pointer to the current directory
-    for x in folders:
-        if x == "":
-            continue
-        for y in path.subFolders:
-            if y.name == x:
-                path = y
-                directories.append(y.name)
-                break
+    temp = ""
     # remove duplicate slashes
-    idx = 0
-    while idx < len(new_directory):
-        if idx - 1 >= 0 and new_directory[idx - 1] == '/' and new_directory[idx] == '/':
-            new_directory = new_directory[:idx] + new_directory[idx + 1:]
-        else:
-            idx += 1
+    for idx in range(0, len(new_directory)):
+        if idx > 0 and new_directory[idx] == '/' and new_directory[idx - 1] == '/':
+            continue
+        temp += new_directory[idx]
+
+    new_directory = temp
+    new_directories = new_directory.split("/")
+
+    # check if the new directory is valid
+    for directory in new_directories:
+        if len(directory) == 0:
+            continue
+        if directory == ".":
+            continue
+        if directory == "..":
+            continue
+        for c in directory:
+            if '0' <= c <= '9':
+                continue
+            if 'a' <= c <= 'z':
+                continue
+            if "A" <= c <= 'Z':
+                continue
+            return "No such file or directory"
+
+    directories = []
+
+    new_directories = new_directory.split("/")
     # check if the new directory is an absolute path
-    if new_directory[0] == '/':
-        new_directory = new_directory[1:]
-        folders = new_directory.split("/")
-        directories = []
-    else:
-        folders = new_directory.split("/")
-    # traverse the folders
-    for i in folders:
-        if len(i) == 0:
+    if new_directory[0] != '/':
+        curr_directories = current_directory.split("/")
+        for s in curr_directories:
+            if len(s) == 0:
+                continue
+            directories.append(s)
+    # parse directories
+    for next_directory in new_directories:
+        if len(next_directory) == 0:
             continue
-        if i == ".":
+        if next_directory == ".":
             continue
-        elif i == "..":
-            if path.parent is not None:
-                path = path.parent
+        elif next_directory == "..":
             if len(directories) > 0:
                 directories.pop(len(directories) - 1)
         else:
-            flag = True
-            for p in path.subFolders:
-                if p.name == i:
-                    directories.append(p.name)
-                    path = p
-                    flag = False
-                    break
-            if flag:
-                return "No such file or directory"
-    # construct the result
+            directories.append(next_directory)
+
+    # construct the result from the list
     res = "/"
-    for i in range(len(directories)):
-        curr = directories[i]
+    for i in range(0, len(directories)):
         if i == 0:
-            res += curr
+            res += directories[i]
         else:
-            res += "/" + curr
+            res += "/" + directories[i]
 
     return res
 
